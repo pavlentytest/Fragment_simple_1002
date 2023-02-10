8eb70ed31da6033fc73486e43ebafc681a81deb8
Pavel <impetus@yandex.ru>
2023-02-10 10:16:34 +0300

    Fixes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var  fm: FragmentManager
 
@@ -16,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener{ loadFragment(FirstFragment())}
-        binding.button2.setOnClickListener{ loadFragment(SecondFragment())}
+        binding.apply {
+            button.setOnClickListener(this@MainActivity)
+            button2.setOnClickListener(this@MainActivity)
+        }
         fm = supportFragmentManager
     }
 
@@ -27,4 +30,12 @@ class MainActivity : AppCompatActivity() {
         ft.commit()
     }
 
+    override fun onClick(p0: View?) {
+        if (p0 != null) {
+            if(p0.id == R.id.button) loadFragment(FirstFragment()) else loadFragment(SecondFragment())
+        }
+    }
+
 }
+
+
